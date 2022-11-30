2bd99972bf9cf364b37ba450928c5d6742662808
Erdin Hermawan <erdinhermawann@gmail.com>
2022-11-30 16:36:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+	swcMinify: true,
 	images: {
 		domains: ["1000logos.net", "avatars.githubusercontent.com", "platform-lookaside.fbsbx.com"],
 	},
